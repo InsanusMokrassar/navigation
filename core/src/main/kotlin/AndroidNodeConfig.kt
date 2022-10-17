39befd406b6eec8f00818f8da3ff5d91d7bb2809
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-10-17 20:02:06 +0600

    return AndroidNodeConfig as deprecated

---
+++ core/src/main/kotlin/AndroidNodeConfig.kt
@@ -0,0 +1,4 @@
+package dev.inmo.navigation.core
+
+@Deprecated("Replaced", ReplaceWith("NavigationNodeDefaultConfig", "dev.inmo.navigation.core.NavigationNodeDefaultConfig"))
+typealias AndroidNodeConfig = NavigationNodeDefaultConfig
