ba2485428274c02e97ca4dbf7613cf3c476e1f45
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-11-30 17:10:20 +0600

    temporal commented UrlsNavigationConfigsRepo

--- core/src/jsMain/kotlin/urls/UrlsNavigationConfigsRepo.kt
+++ core/src/jsMain/kotlin/urls/UrlsNavigationConfigsRepo.kt
@@ -3,14 +3,14 @@ package dev.inmo.navigation.core.urls
 import dev.inmo.navigation.core.repo.ConfigHolder
 import dev.inmo.navigation.core.repo.NavigationConfigsRepo
 
-class UrlsNavigationConfigsRepo<T>(
-
-) : NavigationConfigsRepo<T> {
-    override fun save(holder: ConfigHolder<T>) {
-        TODO("Not yet implemented")
-    }
-
-    override fun get(): ConfigHolder<T>? {
-        TODO("Not yet implemented")
-    }
-}
+//class UrlsNavigationConfigsRepo<T>(
+//
+//) : NavigationConfigsRepo<T> {
+//    override fun save(holder: ConfigHolder<T>) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun get(): ConfigHolder<T>? {
+//        TODO("Not yet implemented")
+//    }
+//}
