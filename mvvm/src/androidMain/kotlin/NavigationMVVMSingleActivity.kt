fb280d41b73013eeaa3a3a79eeb64d06c8d1b658
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-03-12 18:22:05 +0600

    improvements

--- mvvm/src/androidMain/kotlin/NavigationMVVMSingleActivity.kt
+++ mvvm/src/androidMain/kotlin/NavigationMVVMSingleActivity.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import dev.inmo.micro_utils.coroutines.doInUI
 import dev.inmo.micro_utils.coroutines.launchSafelyWithoutExceptions
 import dev.inmo.micro_utils.koin.getAllDistinct
+import dev.inmo.navigation.core.AndroidNavigationHost
 import dev.inmo.navigation.core.configs.NavigationNodeDefaultConfig
 import dev.inmo.navigation.core.fragments.transactions.FragmentTransactionConfigurator
 import dev.inmo.navigation.core.navigationTag
@@ -17,13 +18,13 @@ import org.koin.core.component.KoinComponent
 
 abstract class NavigationMVVMSingleActivity : AppCompatActivity(), KoinComponent {
     protected open val fragmentTransactionConfigurator: FragmentTransactionConfigurator<NavigationNodeDefaultConfig>? = null
+    protected var currentAndroidNavigationHost: AndroidNavigationHost<NavigationNodeDefaultConfig>? = null
     protected open fun allocateLoadingFragment(): Fragment? = null
     protected abstract fun createInitialConfig(rootFragmentTag: String): NavigationNodeDefaultConfig
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rootFragmentTag = findViewById<View>(R.id.main_activity_main_fragment).navigationTag.toString()
 
         val loadingFragment = allocateLoadingFragment()
         if (loadingFragment != null) {
@@ -31,6 +32,8 @@ abstract class NavigationMVVMSingleActivity : AppCompatActivity(), KoinComponent
                 replace(R.id.main_activity_main_fragment, loadingFragment)
             }.commitNow()
         }
+
+        val rootFragmentTag = findViewById<View>(R.id.main_activity_main_fragment).navigationTag.toString()
 
         CoroutineScope(Dispatchers.Default).launchSafelyWithoutExceptions {
             onStartNavigation(rootFragmentTag)
@@ -47,7 +50,8 @@ abstract class NavigationMVVMSingleActivity : AppCompatActivity(), KoinComponent
 
     open suspend fun onStartNavigation(rootFragmentTag: String) {
         doInUI {
-            initNavigation<NavigationNodeDefaultConfig>(
+            currentAndroidNavigationHost ?.job ?.cancel()
+            currentAndroidNavigationHost = initNavigation<NavigationNodeDefaultConfig>(
                 createInitialConfig(rootFragmentTag),
                 getKoin().getAllDistinct<NavigationFragmentInfoProvider>(),
                 manualHierarchyCheckerDelayMillis = 20L,
